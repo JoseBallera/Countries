0465932838b403420a25686df7ca498c8482cee6
Joseballer <joseballera@gmail.com>
2023-10-30 13:48:55 -0400

    peleando con la db

--- server/src/controllers/countryControllers.js
+++ server/src/controllers/countryControllers.js
@@ -1,33 +1,53 @@
 const axios = require("axios");
-const { Country , Activity} = require("../db.js");
+const { Country, Activity } = require("../db.js");
 const { Op } = require("sequelize");
 
 const loadCountriesIntoDB = async () => {
-  const response = await axios.get('http://localhost:5000/countries');
+  const response = await axios.get("http://localhost:5000/countries");
   const countries = response.data.map(country => ({
     ID: country.cca3,
     Nombre: country.name.official,
     Bandera: country.flags.svg,
     Continente: country.region,
-    Capital: country.capital,
+    Capital: country.capital[0],
     Subregion: country.subregion,
     Area: country.area,
-    Poblacion: country.population
+    Poblacion: country.population,
   }));
   await Country.bulkCreate(countries);
 };
 
- loadCountriesIntoDB();
-
 const getAllCountries = async () => {
-  const countries = await Country.findAll({
+  let countries = await Country.findAll({
     include: Activity,
     attributes: { exclude: ["createdAt", "updatedAt"] },
   });
+
+  // Convert model instances to plain JavaScript objects
+  countries = countries.map(country => country.get());
+
+  // console.log(countries);
+  console.log(countries.length);
+  if (countries.length === 0) {
+    await loadCountriesIntoDB();
+
+    countries = await Country.findAll({
+      include: Activity,
+      attributes: { exclude: ["createdAt", "updatedAt"] },
+    });
+
+    // Convert model instances to plain JavaScript objects
+    countries = countries.map(country => country.get());
+
+    //console.log(countries);
+  }
+  
   return countries;
 };
 
+
 const getCountryByName = async (name) => {
+  
   const country = await Country.findAll({
     where: { name: { [Op.iLike]: `%${name}%` } },
     include: Activity,
@@ -37,13 +57,14 @@ const getCountryByName = async (name) => {
 };
 
 const getCountryById = async (id) => {
-  const country = await Country.findByPk(id, {
+  const idToUppercase = id.toUpperCase();
+  console.log(idToUppercase);
+  const country = await Country.findByPk(idToUppercase, {
     include: Activity,
     attributes: { exclude: ["createdAt", "updatedAt"] },
   });
+  console.log(country);
   return country;
 };
-
-
 
 module.exports = { getAllCountries, getCountryByName, getCountryById };
